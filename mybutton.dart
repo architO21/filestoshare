082ab4adce4cb0e061519508219ba8417bfef9d6
Archit Bajpai <97817943+architO21@users.noreply.github.com>
2024-02-04 22:15:52 +0530

    Update mybutton.dart

--- mybutton.dart
+++ mybutton.dart
@@ -8,22 +8,22 @@ class MyButton extends StatelessWidget {
   var buttonPressed;
   MyButton(
       {super.key,
-      required this.buttonText,
-      required this.buttonColor,
-      required this.buttonPressed});
+      required this.buttonText,// we are basically building a container as a button
+      required this.buttonColor,// thus we need the name of the button like '1' ,'2' '+' etc then 
+      required this.buttonPressed});// we need its color and finally we need its function that it performs
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        buttonPressed(buttonText);
+    return GestureDetector( // this is a class in flutter used to detect gestures like tap,press,slide etc
+      onTap: () { // in this case we have tapped into the flutter button and we will perform the function mentioed in
+        buttonPressed(buttonText); // the button with the text as a parameter
       },
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.2,
+        width: MediaQuery.of(context).size.width * 0.2, // explained earlier
         height: MediaQuery.of(context).size.height * 0.1,
         color: buttonColor,
-        child: Center(
-            child: Text(
+        child: Center( // Center is a very useful wifget used when we have to center our items
+            child: Text( 
           softWrap: false,
           maxLines: 1,
           overflow: TextOverflow.fade,
